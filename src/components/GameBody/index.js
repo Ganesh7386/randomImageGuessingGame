eed9c1c1a5944110b2058cde117c148ab701894a
ganesh <106129012+Ganesh7386@users.noreply.github.com>
2023-12-16 13:21:29 +0530

    Updated GameBody/index.js

--- src/components/GameBody/index.js
+++ src/components/GameBody/index.js
@@ -5,7 +5,7 @@ import EachImage from '../eachImageComponent/index'
 class GameBody extends Component {
   state = {
     activeTabId: 'FRUIT',
-    randomSelectedImageId: '1edac278-8390-4da9-b914-5f41fb49283c',
+    randomSelectedImageId: 'b11ec8ce-35c9-4d67-a7f7-07516d0d8186',
     randomIndex: 0,
   }
 
@@ -16,12 +16,12 @@ class GameBody extends Component {
   randomIndexGeneration = () => {
     const {imagesList} = this.props
     const randomIndex = Math.floor(Math.random() * imagesList.length)
-    const {imageId} = imagesList[randomIndex]
-    console.log(imageId)
+    const {id} = imagesList[randomIndex]
+    console.log(id)
 
     this.setState({
       randomIndex,
-      randomSelectedImageId: imageId,
+      randomSelectedImageId: id,
     })
   }
 
@@ -44,7 +44,9 @@ class GameBody extends Component {
     if (imageId === randomSelectedImageId) {
       updateScore()
       this.randomIndexGeneration()
+      console.log('Image matched')
     } else {
+      console.log('Un matched')
       this.randomIndexGeneration()
     }
   }
